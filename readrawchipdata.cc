1460209516f2d5120a49f9ad584c76c566915803
rolfs <rolfs@nikhef.nl>
2013-03-18 23:07:31 +0100

    New macro to read only the Relaxd data and build the tree

---
+++ readrawchipdata.cc
@@ -0,0 +1,279 @@
+// Adjusted from readrawdata.cc to only
+// consider Relaxd data.
+
+//#define  READ2_CC_DEBUG
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TF1.h"
+#include "TString.h"
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+
+using namespace std ;
+#include "GridPix.hpp"
+
+
+// Define which data is valid (Now for 1 chip, not 4!).
+inline bool datavalid(int x, int y, int t){
+	return ( (x>=0) && (x<255) && (y>=0) && (y<255) && (t>=0) && (t<12000) ) ;
+}
+
+// Define root tree header (additional info).
+class EventHeader{
+	public:
+		bool isvalid ;
+		struct _databuf{
+			int evcount ;
+			int evid ;
+			double date ;
+			double frame ;
+			unsigned int errors ;
+		} ;
+		struct _databuf databuf ;
+
+	public:
+		EventHeader (int firstcount = 0) : isvalid(false){
+			databuf.evcount = firstcount ;
+		}
+		void Reset(int firstcount = 0){
+			databuf.evcount = firstcount ;
+		}
+
+		// If header is invalid filled -1,
+		// if isvalid is true return databuf.evcount,
+		// (compressed if else statement -> ?).
+		int GetCount() const { return (isvalid ? databuf.evcount : -1); }
+
+		// Function that retrieves pointer to the data buffer.
+		void* GetDataBuffer() { return &(databuf.evcount); }
+
+		// Tells how the structure is defined.
+		const char* GetDataDescription() const {
+			return "evcount/I:evid/I:date/D:frame/D:errors/i" ;
+		}
+
+		void Update(char* headerstr) ;
+} ;
+
+void EventHeader::Update(char* headerstr){
+  isvalid = false ;
+  databuf.evcount++ ;
+  databuf.errors = 0 ;
+ 
+  // Read in the HEADER.
+	// Assume it wrong till everything went fine and isvalid becomes true.
+	// Otherwise return.
+
+  const char* keyword[3] = {"Eventnr ", "Start time : ", "Acq time : "} ;
+  size_t keywordsize[3] = {8, 13, 11} ;
+
+  // Event number.
+  char* pos = std::strstr(headerstr, keyword[0]);
+  if (!pos ||
+			std::sscanf( &(pos[keywordsize[0]]), "%d", &databuf.evid) != 1){
+		std::cout << "Problems finding the pattern '" << keyword[0]
+			<< "' in the header string:\n" << headerstr << std::endl ;
+		return ;
+	}
+
+  // Start time.
+  pos = std::strstr(headerstr, keyword[1]) ;
+  if (!pos ||
+			std::sscanf( &(pos[keywordsize[1]]), "%lg", &databuf.date) != 1){
+		std::cout << "Problems finding the pattern '" << keyword[1] 
+			<< "' in the header string:\n" << headerstr << std::endl ;
+		return ;
+	}
+
+  // Acquisition time.
+  pos = std::strstr(headerstr, keyword[2]) ;
+  if (!pos ||
+			std::sscanf( &(pos[keywordsize[2]]), "%lg", &databuf.frame) != 1){
+		std::cout << "Problems finding the pattern '" << keyword[2]
+			<< "' in the header string:\n" << headerstr << std::endl ;
+		return ;
+	}
+
+  isvalid = true ;
+}
+
+/****** MAIN FUNCTION ******/
+
+/* Directory + file name ; output file name ; number of events in file */
+int readrawdata(char * relaxdfilepath, char * outrootfile, int readmaxevent ){
+  // Parameters.
+  const int averagehits = 30000 ;
+
+  // Opening files...
+
+  std::ifstream relaxddata(relaxdfilepath) ;
+
+  if (!relaxddata.is_open()){
+		std::cout << "\nThe relaxd data file does not exists!" << std::endl ;
+		return 0 ;
+	}
+  else
+		std::cout << "\nReading data from the file " << relaxdfilepath << "\n" ;
+	
+  TFile rootf(outrootfile,"RECREATE") ;
+
+  if (!rootf.IsOpen()){
+		std::cout << "The output root file cannot be opened! (Check path and writing rights!)\n" << std::endl ;
+		return 0 ;
+	}
+  else
+		std::cout << "The tree will be saved in " << outrootfile << "\n" << std::endl ;
+
+  // Make histograms.
+	TH2F hpixcount("hpixcount","Integral of counts on pixels", 256, -0.5, 255.5, 256, -0.5, 255.5) ;
+  TH2F hbadpix("hbadpix","Pixels with invalid values", 256, -0.5, 255.5, 256, -0.5, 255.5) ;
+ 
+	// Define tree.
+  TTree tree("tree","GridPix data tree") ;
+
+	// Create class Eventheader.
+  class EventHeader eventheader ;
+  //EventHeader eventheader;
+
+	// GridPix class, pointer to it because it's going to be in the tree.
+  GridPix* det1 = new GridPix(averagehits) ;
+
+	// Create tree's branches.
+  tree.Branch("Header",eventheader.GetDataBuffer(), eventheader.GetDataDescription()) ;
+  tree.Branch("det1",&det1) ;
+
+
+  /////////////////////////////////////////
+  //
+  // MAIN LOOP OVER EVENTS
+  //
+  //////////////////////////////////////////
+	
+	TH1D* hTime= new TH1D("hTime","time spectrum;time (clock cycles);entries",11811,0,11811) ;
+
+  char c = 0 ;
+  while ( (relaxddata.get(c)).good() ){
+		// EACH CYCLE THE FIRST CHARACTER OF THE LINE IS TESTED (WHITESPACE -> SKIP, '#' -> HEADER, DIGIT -> DATA).
+		
+		if (std::isspace(c)) continue ; // NEXT CYCLE.
+
+		// IS IT THE HEADER?
+		if (c == '#' && relaxddata.get(c) && c == '-'){
+			char dummy[100] ;
+			if ( !relaxddata.getline(&(dummy[0]),100).good()) { break ; }// ;
+			char dataline[10000] ;
+			size_t npos = 0 ; // Why was `relaxddata.gcount()-1 ;` ?!?!?
+			for (int i=1; i<10; i++){
+				relaxddata.getline(&(dataline[npos]),1000) ;
+				npos += relaxddata.gcount()-1 ;
+			}
+
+			// Fill the previous event.
+			if (eventheader.GetCount() >= 0){
+				// Repack gridpix data, due to usage of SPLIT.
+				det1->RepackData() ;
+				// Now that every information of the previous event is ready, fill the tree.
+				tree.Fill() ;
+			}
+	  
+		  // Some printout every bunch of event.
+		  if (eventheader.GetCount() % 10 == 0){
+				// \r to rewrite the same line (instead of \n). On mac this is different.
+				std::cout << "Processed " << eventheader.GetCount() << " events so far\r" << std::flush ;
+			}
+
+			// Prepare for the next event.
+	  	eventheader.Update(dataline) ;
+	  	det1->ClearData() ;
+	  	continue ; // NEXT CYCLE.
+		}
+
+		// IS IT A DIGIT OF DATA? LET'S ASSUME THAT MANY DATA ARE
+		// FOLLOWING AND LOOP UNTIL A NON-DIGIT CHARACTER IS FOUND.
+		if (std::isdigit(c)){
+			// Putback the already extracted data point.
+			relaxddata.putback(c) ; // The digit is part of the data!
+
+			int x,y,t ;
+			while ( ( relaxddata >> x >> y >> t ).good() ){
+#ifdef READ2_CC_DEBUG
+				std::cout << "\nRead a hit: " << x << "\t" << y << "\t" << t << std::flush ;
+	      //sleep(1) ;
+#endif
+	      // IF VALID insert data into gridpix object 
+				// (data is saved when a new header is found).
+	      if (datavalid(x,y,t)){
+					det1->Insert(x,y,t) ;
+					if (t<11000) hTime->Fill(t) ;	// Already some simple cut on early events.
+					hpixcount.Fill(x,y) ;
+				}
+	      else hbadpix.Fill(x,y) ;
+			}
+
+			// Can be !good() because a '#' (i.e. a new header) is found
+			// instead of a digit(), so the error must be reset and the
+			// main cycle will check it again.
+			relaxddata.clear() ;
+		}
+
+		// AN ESCAPE LANE IN CASE OF BUGS.
+		if (eventheader.GetCount() >= readmaxevent) break ;
+	}	// End of loop over events.
+  
+  // SAVE LAST EVENT (if valid).
+  if (eventheader.GetCount() >= 0){
+		det1->RepackData() ;
+		tree.Fill() ;
+	}
+	
+	std::cout << "DONE! Processed " << eventheader.GetCount() << " events\n" << std::endl ;
+
+  tree.Print() ;
+  tree.Write() ;
+  hbadpix.Write() ;
+  hpixcount.Write() ;
+
+  Double_t max=hTime->GetMaximumBin() ;
+  TF1 *fit=new TF1("fit","gaus",max-50.,max+50.) ;
+  hTime->Fit(fit, "R&&Q") ;
+  hTime->Write() ;
+
+  rootf.Save() ;
+
+  return eventheader.GetCount() ;
+}
+
+
+/************************************************************** 
+YOU CAN THEN REOPEN THE FILE AND TRY THE FOLLOWING IN THE COMMAND
+LINE:
+
+
+tree1.Print()
+tree1.Scan("det1_nhit:det1_x:det1_y:det1_t")
+tree1.Scan("n:x:y:t")
+tree1.Draw("x:y:t")
+tree1.Draw("x:y","t","lego2")
+tree1.Draw("x:y","t*(X>50)","lego2")
+tree1.Draw("x:y","t*(x>50)","lego2")
+tree1.Draw("x:y","","box")
+
+tree2.Print()
+tree2.Scan("det1")
+tree2.Scan("det1.first:det1.second")
+tree2.Scan("int(det1.first/256):det1.first%256:det1.second")
+tree2->SetAlias("det1x","det1.first%256")
+tree2->SetAlias("det1y","int(det1.first/256)")
+tree2.Scan("det1y:det1x:det1.second")
+tree2.Scan("det1y:det1x:det1.second","det1y==15")
+tree2.Scan("det1y:det1x:det1.second","det1x==11")
+
+***************************************************************/
+
